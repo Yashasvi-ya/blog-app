4ab9d095f26d99e8cb6927ea1e6af290c1a904d6
Yashasvi <yashasvi123.yy@gmail.com>
2024-10-01 18:35:22 +0530

    added dashboard and home page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import CallToAction from '../components/CallToAction';
 import CommentSection from '../components/CommentSection.jsx';
-// import PostCard from '../components/PostCard';
+import PostCard from '../components/PostCard';
 
 export default function PostPage() {
   const { postSlug } = useParams();
