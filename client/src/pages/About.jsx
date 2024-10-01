e3b027c7995d52e69a7cc6302d7ae94f6eb25df9
Yashasvi <yashasvi123.yy@gmail.com>
2024-10-01 19:52:42 +0530

    added about and projects page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>
-      about
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About People' Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to People Blog, where we celebrate the diverse stories and
+              experiences that shape our world. Here, we believe that every
+              individual has a unique voice worth sharing. Our mission is to
+              create a vibrant community that fosters connection, understanding,
+              and inspiration through the power of storytelling. Whether you're
+              seeking to learn, engage, or simply enjoy a good read, you'll find
+              a treasure trove of perspectives that reflect the rich tapestry of
+              human life.
+            </p>
+
+            <p>
+              At People Blog, we cover a wide range of topics, from personal
+              narratives and cultural insights to social issues and creative
+              pursuits. Our contributors come from all walks of life, each
+              bringing their own background and expertise to the table. This
+              blend of voices ensures that our content is not only varied but
+              also deeply resonant. We invite you to explore thought-provoking
+              articles, engaging interviews, and relatable anecdotes that
+              reflect the joys and challenges of everyday life.
+            </p>
+
+            <p>
+              Join us on this journey as we delve into the heart of what it
+              means to be human. Whether you're a lifelong reader or new to our
+              community, we encourage you to share your thoughts and
+              experiences. Together, we can amplify voices, spark conversations,
+              and inspire change, one story at a time. Welcome to the People
+              Blog family—your story matters here!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
